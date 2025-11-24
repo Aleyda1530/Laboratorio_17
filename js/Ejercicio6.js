506c0136da5f7c476a702b0c4e1d8b29f548f9ae
Aleyda <aquispehach@unsa.edu.pe>
2025-11-24 17:05:52 -0500

    Ejercicio 6: Función validarEdad con manejo de rrores

---
+++ js/Ejercicio6.js
@@ -0,0 +1,13 @@
+// Ejercicio 6 – Función validarEdad con manejo de errores
+// Autora: Aleyda
+function validarEdad(edad) {
+    if(isNaN(edad) || edad < 0)
+        throw new Error("Edad inválida");
+    return "Edad válida: " + edad;
+}
+try{
+    let resultado = validadEdad(-19);
+    console.log(resultado);
+} catch (error){
+    console.log("Ocurrió un error ", error.message);
+}
